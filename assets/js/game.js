27248c6fd6c929bb57c36d7127ccb3dd86e22918
kenput3r <devolve.wtf@gmail.com>
2017-07-06 19:29:58 -0700

    Added .off() to remove click function from selectHero()

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,13 +11,14 @@ function selectHero() {
 	$('.select-hero').click(function() {
 		var player = $(this).clone();
 		$('#Hero').html(player);
+		$('.select-hero').off('click');
 		$(this).removeClass('select-hero');
 		$(this).remove();
 		$('.select-hero').addClass('select-enemy').removeClass('select-hero');
 		//$('#Players').append(selectedPlayer);
 		//selectedPlayer = player;
 		//console.log(selectedPlayer);
-		$('.select-hero').unbind('click');
+
 	});
 
 }
