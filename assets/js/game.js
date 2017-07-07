1769dc879e96f4ec3ad9b4ece534dde8645db880
kenput3r <devolve.wtf@gmail.com>
2017-07-06 19:19:59 -0700

    Started click function for hero select

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,23 @@
-console.log("hello world");
+var selectedPlayer = '';
+
+$(document).ready(function() {
+
+	selectHero();
+
+});
+
+function selectHero() {
+
+	$('.select-hero').click(function() {
+		var player = $(this).clone();
+		$('#Hero').html(player);
+		$(this).removeClass('select-hero');
+		$(this).remove();
+		$('.select-hero').addClass('select-enemy').removeClass('select-hero');
+		//$('#Players').append(selectedPlayer);
+		//selectedPlayer = player;
+		//console.log(selectedPlayer);
+		$('.select-hero').unbind('click');
+	});
+
+}
