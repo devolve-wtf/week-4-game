b9be3d36ee9d7c6d4bb18bb82b0501db2ccfe067
michael mason <michael@michaeljmason.com>
2017-07-07 12:32:39 -0700

    restructure

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,25 +1,45 @@
-var selectedPlayer = '';
+// var selectedPlayer = '';
+
+// $(document).ready(function() {
+
+// 	selectHero();
+
+// });
+
+// function selectHero() {
+
+// 	$('.select-hero').click(function() {
+// 		var player = $(this).clone();
+// 		player.removeClass('select-hero');
+// 		$('#Hero').html(player);
+// 		$('.select-hero').off('click');
+// 		$(this).removeClass('select-hero');
+// 		$(this).remove();
+// 		$('.select-hero').addClass('select-enemy').removeClass('select-hero');
+// 		//$('#Players').append(selectedPlayer);
+// 		//selectedPlayer = player;
+// 		//console.log(selectedPlayer);
+
+// 	});
+
+// }
+
 
 $(document).ready(function() {
 
-	selectHero();
 
-});
+	$('.select-player').click(function(){
 
-function selectHero() {
+		if($('#hero').html() == false) {
+			$('#hero').html($(this).clone().addClass('active-hero'));
+			$(this).remove();
+		} else if ($('#enemy').html() == false) {
+			$('#enemy').html($(this).clone().addClass('active-enemy'));
+			$(this).remove();
+		}
 
-	$('.select-hero').click(function() {
-		var player = $(this).clone();
-		player.removeClass('select-hero');
-		$('#Hero').html(player);
-		$('.select-hero').off('click');
-		$(this).removeClass('select-hero');
-		$(this).remove();
-		$('.select-hero').addClass('select-enemy').removeClass('select-hero');
-		//$('#Players').append(selectedPlayer);
-		//selectedPlayer = player;
-		//console.log(selectedPlayer);
 
 	});
 
-}
+
+});
