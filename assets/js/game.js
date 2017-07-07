a7bc3b811fd87ce7dcbe4545ac74154921d8096d
michael mason <michael@michaeljmason.com>
2017-07-07 13:59:54 -0700

    attack messages

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,46 +1,11 @@
-// var selectedPlayer = '';
-
-// $(document).ready(function() {
-
-// 	selectHero();
-
-// });
-
-// function selectHero() {
-
-// 	$('.select-hero').click(function() {
-// 		var player = $(this).clone();
-// 		player.removeClass('select-hero');
-// 		$('#Hero').html(player);
-// 		$('.select-hero').off('click');
-// 		$(this).removeClass('select-hero');
-// 		$(this).remove();
-// 		$('.select-hero').addClass('select-enemy').removeClass('select-hero');
-// 		//$('#Players').append(selectedPlayer);
-// 		//selectedPlayer = player;
-// 		//console.log(selectedPlayer);
-
-// 	});
-
-// }
-
 function playerNames(array) {
+
 	var i = 0;
-	$('.player').each(function(){
-		$(this).attr('data-name', array[i])
+	$('.player h2').each(function(){
+		$(this).attr('data-name', array[i]);
+		$(this).text($(this).attr('data-name'));
 		i++;
 	});
-}
-
-function handlePlayerClick() {
-
-	if($('#hero').html() == false) {
-		$('#hero').html($(this).clone().addClass('active-hero'));
-		$(this).remove();
-	} else if ($('#enemy').html() == false) {
-		$('#enemy').html($(this).clone().addClass('active-enemy'));
-		$(this).remove();
-	}
 
 }
 
@@ -59,26 +24,57 @@ function attackPoints() {
 	$('.player').each(function(){
 		$(this).attr('data-attack', Math.floor(Math.random() * 25) + 25);
 	});
+
+}
+
+function handlePlayerClick() {
+
+	if($('#hero').html() == false) {
+		$('#hero').html($(this).clone().addClass('active-hero'));
+		$(this).remove();
+	} else if ($('#enemy').html() == false) {
+		$('#enemy').html($(this).clone().addClass('active-enemy'));
+		$(this).remove();
+	}
+
 }
 
 function attackClick() {
+
 	var enemyHP = parseInt($('.active-enemy .hp').attr('data-hp'));
 	var heroHP = parseInt($('.active-hero .hp').attr('data-hp'));
 	var enemyAttack = parseInt($('.active-enemy').attr('data-attack'));
 	var heroAttack = parseInt($('.active-hero').attr('data-attack'));
 
-	$('.active-enemy .hp').text(enemyHP - heroAttack);
+	$('.active-enemy .hp').text((enemyHP - heroAttack) + ' hp');
 	$('.active-enemy .hp').attr('data-hp', enemyHP - heroAttack);
+	$('.hero-message').text(
+		$('.active-hero h2').attr('data-name') + 
+		' attacked ' + 
+		$('.active-enemy h2').attr('data-name') + 
+		' and reduced their health by ' + 
+		heroAttack + 
+		' points!'
+	);
 
 	setTimeout( function () {
-		$('.active-hero .hp').text(heroHP - enemyAttack);
+		$('.active-hero .hp').text((heroHP - enemyAttack) + ' hp');
 		$('.active-hero .hp').attr('data-hp', heroHP - enemyAttack);
+		$('.enemy-message').text(
+			$('.active-enemy h2').attr('data-name') + 
+			' attacked ' + 
+			$('.active-hero h2').attr('data-name') + 
+			' and reduced their health by ' + 
+			enemyAttack + 
+			' points!'
+		);
 	}, 500);
-
 
 }
 
+
 $(document).ready(function() {
+
 	playerNames(['synaptic', 'merc', 'warfighter', 'phantom']);
 	healthPoints();
 	attackPoints();
